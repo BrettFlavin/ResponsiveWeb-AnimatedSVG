1af7b634c5113a926d30c93221ceb14f18ba955d
Brett Flavin <brettflavin@yahoo.com>
2020-11-18 12:01:46 -0800

    removed unused main.js file, added comments and media query responsiveness,

--- main.js
+++
@@ -1,7 +0,0 @@
-const element = document.querySelectorAll("#animated-headline path");
-
-// helper function simply gets length of each headline letter's path - needed
-// to animate each letter with CSS 'stroke-dashArray' property
-for (let i = 0; i < element.length; i++) {
-  console.log("Letter " + i + " is " + element[i].getTotalLength());
-}
